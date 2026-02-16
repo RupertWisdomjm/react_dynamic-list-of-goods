6867179d1c1ee4a010ee1cfd0786d7d0ed27bd77
Rupert Wisdom <rupert.wisdom@yahoo.com>
2026-02-16 13:49:41 -0500

    update the memorized component

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,7 +5,7 @@ type Props = {
   goods: Good[];
 };
 
-export const GoodsList: React.FC<Props> = React.memo(({ goods }) => (
+const GoodsListBase: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
       <li key={good.id} data-cy="good" style={{ color: good.color }}>
@@ -13,4 +13,7 @@ export const GoodsList: React.FC<Props> = React.memo(({ goods }) => (
       </li>
     ))}
   </ul>
-));
+);
+
+export const GoodsList = React.memo(GoodsListBase);
+GoodsList.displayName = 'GoodsList';
